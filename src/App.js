ae6fb7080a6b35b2685972cfe6d70a01d087c710
Unknown <eve_2nd@hotmail.com>
2020-01-30 23:34:44 +0700

    Header_v1.1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>อีควายอัพ</h1>
+          <h1>Hello React</h1>
       </div>
     )
   }
--- src/Component/Header.js
+++ src/Component/Header.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import logo from './logo.jpg';
+
+
 
 export default class Header extends Component {
     render() {
         //JSX
         return (
             <div style={style.Header}>
+                <img src={logo} className="App-logo" alt="logo" size = "30%" />
                 <h1>SORTING AND PICKING SYSTEMS VIA MACHINE LEARNING</h1>
             </div>
         )
@@ -15,8 +19,9 @@ const style ={
     {
         color:'white',
         fontsize : 50,
-        backgroundColor:'#ff0000',
+        backgroundColor:'#2699FB',
         height : 100,
+        padding : 20
     }
     
 }
