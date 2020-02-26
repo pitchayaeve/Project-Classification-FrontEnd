2f9d38dd1cd922b4ef0f52278bb0c864a0ddc486
Unknown <eve_2nd@hotmail.com>
2020-02-27 01:59:26 +0700

    component

---
+++ src/Components/Toggleswitch.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Toggleswitch extends Component {
+  state = {
+    switch1: true,
+  }
+  handleSwitchChange = nr => () => {
+    let switchNumber = `switch${nr}`;
+    this.setState({
+      [switchNumber]: !this.state[switchNumber]
+    });
+  }
+  render() {
+    return (
+      <div className='custom-control custom-switch'>
+        <input
+          type='checkbox'
+          className='custom-control-input'
+          id='customSwitches'
+          checked={this.state.switch1}
+          onChange={this.handleSwitchChange(1)}
+          readOnly
+        />
+        <label className='custom-control-label' htmlFor='customSwitches'></label>
+      </div>
+    );
+  }
+}
+export default Toggleswitch;
+//React Bootstrap Switch -> https://mdbootstrap.com/docs/react/forms/switch/
