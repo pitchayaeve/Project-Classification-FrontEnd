df1d4a6391efc742cff6f54fb037232ce3c23f00
Unknown <eve_2nd@hotmail.com>
2020-02-27 01:58:32 +0700

    octagon

---
+++ src/Octagon.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import "./Octagon.css";
+
+export default class Octagon extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+        <div class="trapezoiddown"/>
+
+        {/* <div class="trapezoidupleft"/> */}
+        {/* <div class="trapezoidup"/>
+        <div class="trapezoidleft"/>
+        <div class="trapezoiddownleft"/>
+        <div class="trapezoidupright"/>
+        <div class="trapezoidright"/>
+        <div class="trapezoiddownright"/> */}
+        {/* <div class="parallelogram"/> */}
+        {/* <div class="octagon"/> */}
+          {/* <div style={styles.pentagonInner}/>
+          <div style={styles.pentagonBefore}/> */}
+        </div>
+      </div>
+    );
+  }
+}
+
+// const styles = ({
+//   pentagon: {
+//     backgroundColor: "red"
+//   },
+//   pentagonInner: {
+//     width: 90,
+//     borderBottomColor: "red",
+//     borderBottomWidth: 0,
+//     borderLeftColor: "transparent",
+//     borderLeftWidth: 18,
+//     borderRightColor: "transparent",
+//     borderRightWidth: 18,
+//     borderTopColor: "red",
+//     borderTopWidth: 50
+//   },
+//   pentagonBefore: {
+//     position: "absolute",
+//     height: 0,
+//     width: 0,
+//     top: -35,
+//     left: 0,
+//     borderStyle: "solid",
+//     borderBottomColor: "red",
+//     borderBottomWidth: 35,
+//     borderLeftColor: "transparent",
+//     borderLeftWidth: 45,
+//     borderRightColor: "transparent",
+//     borderRightWidth: 45,
+//     borderTopWidth: 0,
+//     borderTopColor: "transparent"
+//   }
+
